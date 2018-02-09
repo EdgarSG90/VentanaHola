3a813c946b6f92aa1b438f3c333505c6ef324114
agesquivel <agesquivel@gmail.com>
2018-02-09 08:17:02 -0500

    Se agregan componentes y Listener (oyente) para botones.

--- src/VentanaLogin.java
+++ src/VentanaLogin.java
@@ -1,14 +1,53 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaLogin extends JFrame{
 
     public VentanaLogin(String titulo) {
         this.setTitle(titulo);
+        this.setResizable(false);
+        this.setLayout(null);
 
         Container panelPrinc = this.getContentPane();
         panelPrinc.setBackground(Color.GREEN);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel lblUsuario = new JLabel("Usuario: ");
+        lblUsuario.setBounds(10, 10, 100, 30);
+        panelPrinc.add(lblUsuario);
+
+        JTextField edtUsuario = new JTextField();
+        edtUsuario.setBounds(120, 10, 100, 30);
+        panelPrinc.add(edtUsuario);
+
+        JLabel lblClave = new JLabel("Contraseña: ");
+        lblClave.setBounds(10, 50, 100, 30);
+        panelPrinc.add(lblClave);
+
+        JTextField edtClave = new JTextField();
+        edtClave.setBounds(120, 50, 100, 30);
+        panelPrinc.add(edtClave);
+
+        //Creación de objetos que generan eventos
+        JButton btnAceptar = new JButton("Aceptar");
+        btnAceptar.setBounds(40, 90, 80, 30);
+        panelPrinc.add(btnAceptar);
+
+        JButton btnCancelar = new JButton("Cancelar");
+        btnCancelar.setBounds(130, 90, 80, 30);
+        panelPrinc.add(btnCancelar);
+
+        //Creación de oyentes
+        AccionBoton oyenteBtnAceptar = new AccionBoton();
+        AccionBoton oyenteBtnCancelar = new AccionBoton();
+
+        //Vincular oyente al componente
+        btnAceptar.addActionListener(oyenteBtnAceptar);
+        btnCancelar.addActionListener(oyenteBtnCancelar);
     }
+
 
     public static void main(String[] args)
     {
@@ -16,8 +55,29 @@ public class VentanaLogin extends JFrame{
         f.setSize(300,300);
         f.setLocation(200,100);
         //f.setBackground(Color.cyan); No funciona Es sobre el panel principal
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         f.setVisible(true);
     }
 }
+
+
+class AccionBoton implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Presionaste un botón " + ((JButton) e.getSource()).getText());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
