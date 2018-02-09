d5da50fe5a8dbc7be6d9ae050065c49a039c79c6
agesquivel <agesquivel@gmail.com>
2018-02-08 19:42:42 -0500

    Ventana se vuelve JFrame

---
+++ src/VentanaLogin.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class VentanaLogin extends JFrame{
+
+    public VentanaLogin(String titulo) {
+        this.setTitle(titulo);
+
+        Container panelPrinc = f.getContentPane();
+        panelPrinc.setBackground(Color.GREEN);
+        
+
+    }
+
+    public static void main(String[] args)
+    {
+        VentanaLogin f = new VentanaLogin("Primera Ventana");
+        f.setSize(300,300);
+        f.setLocation(200,100);
+        //f.setBackground(Color.cyan); No funciona Es sobre el panel principal
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setVisible(true);
+    }
+}
